88f0f8e5da6f8a5de8c89f911b3a04495b98a372
githubloverforever <dmitry.prihodko@rhinoda.com>
2020-12-15 01:16:45 +0500

    Added fetch for products

---
+++ interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface IProduct {
+  content: string,
+  created_at: string,
+  id: number,
+  meta_description: string,
+  meta_title: string,
+  name: string,
+  price: number,
+  published_at: string,
+  slug: string,
+  updated_at: string,
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import style from '../styles/Home.module.css';
+import fetch from 'isomorphic-unfetch';
+import {IProduct} from "../interfaces";
 
-export default function Home() {
+export default function Home({products}: Array<IProduct>) {
   return (
     <div className={style.container}>
      <h1>Hello!</h1>
     </div>
   )
+}
+
+export async function getServerSideProps() {
+  const { API_URL } = process.env;
+
+  const res = await fetch(`${API_URL}/products`)
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data
+    }
+  }
 }
